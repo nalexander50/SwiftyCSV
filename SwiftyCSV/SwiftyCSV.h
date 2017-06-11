b5c28657b27dd469f2abb5a708a328d78967eb18
Nick Alexander <alexander50@live.com>
2017-06-11 14:48:51 -0400

    Initial Commit

---
+++ SwiftyCSV/SwiftyCSV.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyCSV.h
+//  SwiftyCSV
+//
+//  Created by Nick Alexander on 6/11/17.
+//  Copyright © 2017 com.alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyCSV.
+FOUNDATION_EXPORT double SwiftyCSVVersionNumber;
+
+//! Project version string for SwiftyCSV.
+FOUNDATION_EXPORT const unsigned char SwiftyCSVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCSV/PublicHeader.h>
+
+
